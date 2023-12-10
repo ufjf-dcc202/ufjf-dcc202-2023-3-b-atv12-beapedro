1462623f4e681d79814c82590fda3278644b59ab
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-12-10 16:33:50 -0300

    defnindo constantes e funções do main

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 import { getEstoque, transacaoNoEstoque, limpaEstoque } from "./estoque.js";
+const olJoao = document.querySelector("#joao");
+const olMaria = document.querySelector("#maria");
+
+document.entrada.addEventListener('submit', leFormulario);
+document.entrada.reset();
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('buttonLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
 
