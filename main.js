aae625c1ceadf9ceb89bcab9c8c05e3370ba3132
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-12-10 16:54:48 -0300

    alteração no main para atualizar lista

--- main.js
+++ main.js
@@ -37,3 +37,15 @@ function leFormulario(event) {
         }
     }
 }
+function atualizaTela() {
+    const estoque = getEstoque();
+
+    olJoao.innerHTML = "";
+    olMaria.innerHTML = "";
+    document.entrada.quantidade.value = 1;
+    document.entrada.fruta.value = "maca";
+
+    preencheLista(olJoao, estoque.joao);
+    preencheLista(olMaria, estoque.maria);
+    
+}
