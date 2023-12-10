2c54bb88edeff1cf53c5a9945df65adbcb9f2c48
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-12-10 16:47:22 -0300

    definindo func getEstoque e trasacaonoestoque

--- estoque.js
+++ estoque.js
@@ -6,4 +6,30 @@ let estoque = {
       {'tipo': 'maca', 'quantidade': 2},
     ],
   };
+  function getEstoque() {
+    return structuredClone(estoque);
+  }
+  
+  function transacaoNoEstoque(origem, destino, tipo, quantidade) {
+    if (!estoque[origem] && origem !== "pomar") {
+      estoque[origem] = [];
+    }
+  
+    if (!estoque[destino] && destino !== "pomar") {
+      estoque[destino] = [];
+    }
+  
+    if (quantidade < 0 || origem === destino) {
+      return;
+    }
+  
+    if (destino === "pomar") {
+      let itemEncontrado = estoque[origem].find(item => item.tipo === tipo);
+  
+      if (itemEncontrado) {
+        itemEncontrado.quantidade = Math.max(0, itemEncontrado.quantidade - quantidade);
+      }
+  
+      return;
+    }
   
