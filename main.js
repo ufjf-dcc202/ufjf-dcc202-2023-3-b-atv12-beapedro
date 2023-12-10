d5566acd4d676a014bf62009668581ab085a0156
Pedro Augusto Silva Reis <reis.pedro@estudante.ufjf.br>
2023-12-10 16:44:39 -0300

    adicionando outras funções

--- main.js
+++ main.js
@@ -11,4 +11,29 @@ document.addEventListener('DOMContentLoaded', () => {
         atualizaTela();
     });
 });
+atualizaTela();
 
+function leFormulario(event) {
+   event.preventDefault(event);
+   const quantidade = document.entrada.quantidade.valueAsNumber;
+   const fruta = document.entrada.fruta.value;
+   const origem = document.entrada.origem.value;
+   const destino = document.entrada.destino.value;
+   console.log(`${origem} doa ${quantidade} ${fruta} para ${destino}`);
+
+    transacaoNoEstoque(origem, destino, fruta, quantidade);
+   atualizaTela();}
+
+   function preencheLista(lista, estoqueDaPessoa) {
+    lista.textContent = "";
+
+    if (Array.isArray(estoqueDaPessoa)) {
+        for (let i = 0; i < estoqueDaPessoa.length; i++) 
+        {
+            const monte = estoqueDaPessoa[i];
+            const li = document.createElement('li');
+            li.textContent = `${monte.tipo}: ${monte.quantidade}`;
+            lista.appendChild(li);
+        }
+    }
+}
